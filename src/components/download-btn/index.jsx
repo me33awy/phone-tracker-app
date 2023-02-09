4a8af3346725b166a37fc6b6ace2f4c6eb5d5cc5
me33awy <me33awy@gmail.com>
2023-02-09 19:32:55 +0200

    Remove download/index.jsx

--- src/components/download-btn/index.jsx
+++ src/components/download-btn/index.jsx
@@ -1,10 +1,9 @@
 import React, { useRef } from "react";
 import { Button } from "react-bootstrap";
 import "./index.scss";
-import Pdf from "../../assets/pdf/table.pdf";
 const DownloadButton = () => {
   return (
-    <a download={Pdf}>
+    <a>
       <Button className="download-button">Download The App </Button>
     </a>
   );
